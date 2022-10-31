afe6ff4a32954449317770f6c227e62add587dce
Bharath Kumar <bharathkumar@example.com>
2022-10-31 06:50:47 +0530

    bring min, max and sort btn listeners to option.js

--- option.js
+++ option.js
@@ -21,3 +21,59 @@ searchBarInputEle.focus()
 searchBarInputEle.addEventListener('input', (e) => {
     onSearchBarInputChange(e)
 })
+
+//Closed Tabs MinMax Btn
+const minMaxClosetabsBtn = closeTabCntEle.querySelector('.close.tabs-cnt .min-max-btn')
+minMaxClosetabsBtn.addEventListener('click', (e)=>{
+    e.stopPropagation()
+    let closeTabsViewMode
+    chrome.storage.local.get('closeTabsViewMode',(value) =>{
+        closeTabsViewMode = value.closeTabsViewMode
+        if (closeTabsViewMode !== 'C') closeTabsViewMode = 'C'
+        else closeTabsViewMode = 'T'
+        chrome.storage.local.set({closeTabsViewMode})
+        processClosedTabs()
+    })
+})
+//Closed Tabs Sort Btn
+const sortByCloseTabBtn = closeTabCntEle.querySelector('.close.tabs-cnt .sort-by')
+sortByCloseTabBtn.addEventListener('click', (e)=>{
+    e.stopPropagation()
+    let closeTabsViewMode
+    chrome.storage.local.get('closeTabsViewMode',(value) =>{
+        closeTabsViewMode = value.closeTabsViewMode
+        if (['E','C'].includes(closeTabsViewMode)) closeTabsViewMode = 'T'
+        else closeTabsViewMode = 'E'
+        chrome.storage.local.set({closeTabsViewMode})
+        processClosedTabs()
+    })
+})
+
+//Open tabs
+//Min max open Tab Button Click
+const minMaxOpentabsBtn = openTabCntEle.querySelector('.open.tabs-cnt .min-max-btn')
+minMaxOpentabsBtn.addEventListener('click', (e)=>{
+    e.stopPropagation()
+    let openTabsViewMode
+    chrome.storage.local.get('openTabsViewMode',(value) =>{
+        openTabsViewMode = value.openTabsViewMode
+        if (openTabsViewMode !== 'C') openTabsViewMode = 'C'
+        else openTabsViewMode = 'T'
+        chrome.storage.local.set({openTabsViewMode})
+        processOpenTabs()
+    })
+})
+
+const sortByOpenTabBtn = openTabCntEle.querySelector('.open.tabs-cnt .sort-by')
+sortByOpenTabBtn.addEventListener('click', (e)=>{
+    e.stopPropagation()
+    let openTabsViewMode
+    chrome.storage.local.get('openTabsViewMode',(value) =>{
+        openTabsViewMode = value.openTabsViewMode
+        if (['E','C'].includes(openTabsViewMode)) openTabsViewMode = 'T'
+        else openTabsViewMode = 'E'
+        chrome.storage.local.set({openTabsViewMode})
+        processOpenTabs()
+    })
+
+})
